5fd4f115dded096daf1bda8fe30db4d38d90e258
andreev <test@test.com>
2016-12-08 14:36:22 +0500

    chat.js

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -3,32 +3,190 @@ spa.chat = (function () {
 	var 
 		configMap = {
 			main_html : String()
-				+'<div style="padding-left: 1em; color: #fff;">'
-				+	'Say hello to chat'
+				+'<div class="spa-chat">'
+				+	'<div class="spa-chat-head"'
+				+		'<div class="spa-chat-head-toggle">'+'</div>'
+				+		'<div class="spa-chat-head-title">'
+				+			'Chat'
+				+		'</div>'
+				+	'</div>'
+				+	'<div class="spa-chat-closer">x</div>'
+				+	'<div class="spa-chat-sizer">'
+				+		'<div class="spa-chat-msgs"></div>'
+				+		'<div class="spa-chat-box">'
+				+			'<input type="text"/>'
+				+			'<div>send</div>'
+				+		'</div>'
+				+	'</div>'
 				+'</div>',
-			settable_map : {}
-		},
-		stateMap = {$container : null},
-		jqueryMap = {},
-		setJqueryMap, configModule, initModule
-	// Служебные методы
-	// Начало методов DOM
+			settable_map : {
+				slider_open_time  :true,
+				slider_close_time :true,
+				slider_open_em    :true,
+				slider_close_em   :true,
+				slider_open_title :true,
+				slider_close_title:true,
 
-	setJqueryMap = function () {
-		var $container = stateMap.$container;
-		jqueryMap =  {$container : $container};
+				chat_model     :true,
+				people_model   :true,
+				set_chat_anchor:true
+			},
+
+			slider_open_time   :250,
+			slider_close_time  :250,
+			slider_open_em     :16,
+			slider_close_em    :2,
+			slider_open_title  :'Click to close',
+			slider_close_title :'Click to open',
+
+			chat_model     :null,
+			people_model   :null,
+			set_chat_anchor:null
+		},
+		stateMap = {
+			$append_target   :null,
+			position_type    :'closed',
+			px_per_em        :0,
+			slider_hidden_px :0,
+			slider_closed_px :0,
+			slider_opened_px :0
+		},
+		jqueryMap = {},
+		setJqueryMap, getEmSize, setPxSize, setSliderPosition, onClickToggle, configModule, initModule;
+
+	// Служебные методы
+	getEmSize = function (elem) {
+		return Number(
+			getComputedStyle(elem,'').fontSize.match(/\d*\.?\d*/)[0]
+		);
 	}
+
+	// Начало методов DOM
+		// Метод setJqueryMap
+	setJqueryMap = function () {
+		var 
+			$append_target = stateMap.$append_target,
+			$slider = $append_target.find('.spa-chat');
+
+		jqueryMap =  {
+			$slider : $slider,
+			$head   : $slider.find('.spa-chat-head'),
+			$toggle : $slider.find('.spa-chat-head-toggle'),
+			$title  : $slider.find('.spa-chat-head-title'),
+			$sizer  : $slider.find('.spa-chat-sizer'),
+			$msgs   : $slider.find('.spa-chat-msgs'),
+			$box    : $slider.find('.spa-chat-box'),
+			$input  : $slider.find('.spa-chat-input input[type=text]')
+		};
+	};
+		// Метод setPxSizes
+	setPxSizes = function () {
+		var px_per_em, opened_height_em;
+		px_per_em = getEmSize( jqueryMap.$slider.get(0));
+
+		opened_height_em = configMap.slider_close_em;
+
+		stateMap.px_per_em = px_per_em;
+		stateMap.slider_opened_px = opened_height_em * px_per_em;
+		stateMap.slider_closed_px = configMap.slider_close_em * px_per_em;
+		jqueryMap.$sizer.css({
+			height : (opened_height_em - 2) * px_per_em
+		});
+	};
 	// Обработчики событий
+
+	onClickToggle = function (event) {
+		var set_chat_anchor = configMap.set_chat_anchor;
+		if (stateMap.position_type === 'opened') {
+			set_chat_anchor('closed');
+		} else 
+			if (stateMap.position_type === 'closed') {
+				set_chat_anchor('opened');
+			} return false;
+	};
 	// Открытые методы
+		// метод setSliderPosition
+		// Пример : spa.chat.setSliderPosition('closed');
+		// Назначение : установить окно чата в требуемое состояние
+		// Аргументы : 
+		//		position_type - enum('closed', 'opened', 'hidden')
+		//		callback - необязательная функция, вызывается при завершении анимации
+		// Действие : 
+		// 		Оставляет окно чата в текущем состоянии, если новое состояние
+		// 		совпадает с текущим, иначе анимирует переход в новое состоянеи.
+		// Возвращает : 
+		// 		true - запрошенное состояние установлено
+		//		false - запрошенное состояние не установленно
+		// Исключения : нет
+	setSliderPosition = function (position_type, callback) {
+		var 
+			height_px, animate_time, slider_title, toggle_text;
+
+			// вернуть true, если окно чата уже находится в требуемом состоянии
+			if (stateMap.position_type === position_type) {
+				return true;
+			};
+
+			// подготовить пареметры анимации
+			switch(position_type){
+				case 'opened' : 
+					height_px = stateMap.slider_opened_px;
+					animate_time = configMap.slider_open_time;
+					slider_title = configMap.slider_open_title;
+					toggle_text = '=';
+				break;
+
+				case 'hidden' :
+					height_px = 0;
+					animate_time = configMap.slider_open_time;
+					slider_title = '';
+					toggle_text = '+';
+				break;
+
+				case 'closed' :
+					height_px = stateMap.slider_closed_px;
+					animate_time = configMap.slider_close_time;
+					slider_title = configMap.slider_close_title;
+					toggle_text = '+';
+				break;
+
+				// выйдти из метода, если position_type имеет неизвестное значение
+				default : return false;
+			}
+
+			// анимировать изменение состояния окна чата
+			stateMap.position_type = '';
+			jqueryMap.$slider.animate(
+				{ height : height_px },
+				animate_time,
+				function () {
+					jqueryMap.$toggle.prop('title', slider_title);
+					jqueryMap.$toggle.text(toggle_text);
+					stateMap.position_type = position_type;
+					if (callback) { callback(jqueryMap.$slider);}
+				}
+			);
+			return true;
+	};
 
 		// метод configModule
-		// Назначение : настроить допустимые ключи 
-		// Аргумениы : хэш настраиваемых кдючей и их методов
-		// 		color_name - какой цвет использовать
-		// Параметры : 
-		// 		configMap.settable_map - обьявляет допустимые кдючи
+		// Пример : spa.chat.configModule({slider_open_em : 18});
+		// Назначение : сконфигурировать модуль до инициализации
+		// Аргумениы : 
+		// 	  *	set_chat_anchor - обратный вызов для модификации якоря в URI,
+		//		чтобы отразить состояние: открыт или закрыт. Обратный вызов должен
+		//		возвращать false, если установить указанное состояние невозможно.
+		//	  *	chat_model - объект модели chat, котоый предоставляет методы для
+		//		взаимодействия с нашей системой мгновенного обмена сообщениями.
+		//	  *	people_model - объект модели people, который предоставляет
+		//      методы для управления списком пользователей, хранящимся в модели.
+		//	  * параметры slider_*. Все это необязательные скляры.
+		// Действие : 
+		// 		Внутренняя структура, в которой хранятся конфигурационные параметры
+		//      (configMap), обновляется в соответствии с переданными аргументами.
+		//		Больше никаких действий не предпринимается.
 		// Возвращает : true
-		// Исключение : нет
+		// Исключение : объект ошибки
 
 	configModule = function (input_map) {
 		spa.util.setConfigMap({
@@ -40,21 +198,38 @@ spa.chat = (function () {
 	};
 
 		// метод initModule
-		// Назначение: инициализарует модуль
+		// Пример : spa.chat.initModule($('#div_id'));
+		// Назначение: 
+		//      Требует, чтобы Chat начал предоставлять свою функциональность
+		//      пользователю.
 		// Аргументы: 
-		// 		$container - элемент jquery, используемфый этим модулем
-		// Возвращает: true
+		// 		$append_target (example: $('#div_id'));
+		//      Коллекция jQuery, которая должна содержать
+		//		единственный элемент DOM - контейнер
+		// Действие :
+		//		Добавляет выплывающий чат в конец указанного контейнера и заполняет
+		//		его HTML - содержимым. Затем инициализирует элементы, события и обработчики,
+		//		так чтобы предоставить пользователю интерфейс для работы с
+		//		комнатой в чате.
+		// Возвращает: true в случае успеха иначе false
 		// Исключения: none
-	initModule = function ($container) {
-		$container.html(configMap.main_html);
-		stateMap.$container = $container;
+	initModule = function ($append_target) {
+		$append_target.append(configMap.main_html);
+		stateMap.$append_target = $append_target;
 		setJqueryMap();
+		setPxSizes();
+
+		// Установить начальный заголовок окна чата
+		jqueryMap.$toggle.prop('title', configMap.slider_close_title);
+		jqueryMap.$head.click(onClickToggle);
+		stateMap.position_type = 'closed';
 		return true;
 	}
 
 	// вернуть открытые методы
 	return{
-		configModule : configModule,
-		initModule 	 : initModule
+		setSliderPosition : setSliderPosition,
+		configModule      : configModule,
+		initModule 	      : initModule
 	}
 }());
